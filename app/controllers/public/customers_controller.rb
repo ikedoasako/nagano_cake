2a99d8443f1b45f2268a738327b898d918ce6f0c
ikedoasako <124412564+ikedoasako@users.noreply.github.com>
2023-04-30 14:42:38 +0000

    routesの編集

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,9 +1,12 @@
 class Public::CustomersController < ApplicationController
   
   def show
+    @customer = current_customer
   end
 
   def edit
+    @customer = Customer.find(params[:id])
+    redirect_to customer_path(@customer.id)
   end
 
   def update
--- config/routes.rb
+++ config/routes.rb
@@ -1,27 +1,4 @@
 Rails.application.routes.draw do
-
-  namespace :admin do
-    get "/admin" => "homes#top"
-    get 'orders/show'
-    get 'customers/index'
-    get 'customers/show'
-    get 'customers/edit'
-    get 'items/index'
-    get 'items/new'
-    get 'items/show'
-    get 'items/edit'
-  end
-
-  scope module: :public do
-    root to: 'homes#top'
-    get "/home/about" => "homes#about", as: "about"
-    resources :orders
-    resources :cart_items
-    get 'customers/show'
-    get 'customers/edit'
-    get 'items/index'
-    get 'items/show'
-  end
   # 管理者用
   # URL /admin/sign_in ...
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
@@ -33,6 +10,33 @@ Rails.application.routes.draw do
     registrations: "public/registrations",
     sessions: 'public/sessions'
   }
+
+  namespace :admin do
+    get "/admin" => "homes#top"
+    get "admin/customers" => "customers#index"
+    get "admin/customers/:id" => "customers#show"
+    get "admin/customers/:id/edit" => "customers#edit"
+    get "admin/customers/:id" => "customers#update"
+    get "admin/orders/:id" => "orders#show"
+    resources :admin_items
+  end
+
+  scope module: :public do
+    root to: 'homes#top'
+    get "/home/about" => "homes#about", as: "about"
+    get "customers/my_page" => "customers#show"
+    get "customers/information/edit" => "customers#edit"
+    get "customers/information" => "customer#update"
+    get "customers/unsubscribed" => "customer#unsubscribed"
+    get "customers/withdraw" => "customer#withdraw"
+    get "orders/confirm" => "orders#confirm"
+    get "orders" => "orders#create"
+    get "orders" => "orders#index"
+    get "orders/:id" => "orders#show"
+    resources :cart_items
+    resources :items
+  end
+
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
