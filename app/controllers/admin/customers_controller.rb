c5fd398b2694619fa0b26974b35de1d3ae6a6122
ikedoasako <124412564+ikedoasako@users.noreply.github.com>
2023-05-16 15:53:10 +0000

    会員一覧

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,9 +1,12 @@
 class Admin::CustomersController < ApplicationController
 
   def index
+    @customers = Customer.all
+    @customers = Customer.page(params[:page])
   end
 
   def show
+    @customer = Customer.find(params[:id])
   end
 
   def edit
--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -13,7 +13,6 @@ class Admin::ItemsController < ApplicationController
 
   def show
     @item = Item.find(params[:id])
-    
   end
 
   def index
